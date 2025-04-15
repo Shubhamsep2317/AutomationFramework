415a307b5b16584fbbd2ec06309a6cf0b242b583
Shubham <Shubhamsep23@gmail.com>
2025-04-15 21:07:06 +0530

    15th April Framework Creation

--- src/test/java/TestsFromExcel/LoginMultipleUsers.java
+++ src/test/java/TestsFromExcel/LoginMultipleUsers.java
@@ -8,6 +8,8 @@ import Automation.org.Shubham.Utils.PropertiesReader;
 import UtilExcel.UtilExcel;
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
@@ -18,15 +20,20 @@ import static Automation.org.Shubham.Driver.DriverManager.getDriver;
 @Listeners(ScreenshotListener.class)
 public class LoginMultipleUsers extends CommonToAllTests {
 
+    Logger logger=LogManager.getLogger(LoginMultipleUsers.class);
+
     @Owner("Shubham")
     @Description("Verify login via multiple sets of username and password")
     @Test(dataProvider = "getData",retryAnalyzer = RetryAnalyser.class)
     public void verifyLoginWithValidInvalidCredentials(String username, String password) {
 
-        LoginPage loginPage = new LoginPage(getDriver());
-        String error_message = loginPage.loginToVWOLoginInvalidCreds(username, password);
 
+        LoginPage loginPage = new LoginPage(getDriver());
+        logger.info("Test case execution with multiple users starts");
+        String error_message = loginPage.loginToVWOLoginInvalidCreds(username, password);
+        logger.info("Checking expected error to actual error message");
         Assert.assertEquals(error_message, PropertiesReader.readKey("error_message"));
+        logger.info("Test case execution ends");
 
     }
 
