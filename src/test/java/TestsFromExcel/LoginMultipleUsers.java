6a792850319e67ebb30b76b162a27d4e086a6cff
Shubham <Shubhamsep23@gmail.com>
2025-04-15 18:08:30 +0530

    15th April Framework Creation

--- src/test/java/TestsFromExcel/LoginMultipleUsers.java
+++ src/test/java/TestsFromExcel/LoginMultipleUsers.java
@@ -19,23 +19,21 @@ public class LoginMultipleUsers extends CommonToAllTests {
     @Owner("Shubham")
     @Description("Verify login via multiple sets of username and password")
     @Test(dataProvider = "getData")
-    public void verifyLoginWithValidInvalidCredentials(String username, String password){
+    public void verifyLoginWithValidInvalidCredentials(String username, String password) {
 
         DriverManager.init();
 
         getDriver().navigate().to(PropertiesReader.readKey("url"));
 
-        LoginPage loginPage=new LoginPage(getDriver());
-        String error_message=loginPage.loginToVWOLoginInvalidCreds(username, password);
+        LoginPage loginPage = new LoginPage(getDriver());
+        String error_message = loginPage.loginToVWOLoginInvalidCreds(username, password);
 
         Assert.assertEquals(error_message, PropertiesReader.readKey("error_message"));
-
-
 
     }
 
     @DataProvider
-    public Object[][] getData(){
-       return UtilExcel.getExcelDataUtil("Sheet1");
+    public Object[][] getData() {
+        return UtilExcel.getExcelDataUtil("Sheet1");
     }
 }
